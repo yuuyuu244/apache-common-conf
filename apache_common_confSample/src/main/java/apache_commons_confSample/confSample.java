d3cce2e948bd1c902fe4cb15d9542556d3d4d971
yuki-kikuya <yuki-kikuya@SCC41K170340802>
2017-08-21 18:18:19 +0900

    first commit and push

---
+++ apache_common_confSample/src/main/java/apache_commons_confSample/confSample.java
@@ -0,0 +1,46 @@
+package apache_commons_confSample;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+public class confSample {
+    public static void main(String[] args)  {
+//        try {
+            PropertiesConfiguration config;
+            try {
+                config = new PropertiesConfiguration("wps.conf");
+//                Object pagePath = config.getProperty("PAGE_PATH");
+                
+                Iterator<String> keys = config.getKeys();
+                if (keys != null) {
+                keys.forEachRemaining((String key)->{
+                  System.out.println(key);
+                  System.out.println(config.getString(key));
+                });
+                }
+                
+                String commonPagePath = config.getString("COMMON_PAGE_PATH");
+                System.out.println("COMMON_PAGE_PATH="+commonPagePath);
+                
+                
+                List<Object> list = config.getList("PAGE_PATH");
+                Map<String, String> map= new LinkedHashMap<>();
+//                Properties pagePathPro = config.getProperties("PAGE_PATH");
+
+                list.forEach((item) -> {
+                     System.out.println(item.toString());
+                });
+                
+                
+            } catch (ConfigurationException e) {
+                // TODO é©ìÆê∂ê¨Ç≥ÇÍÇΩ catch ÉuÉçÉbÉN
+            }
+    }
+
+}
